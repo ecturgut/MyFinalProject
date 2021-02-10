31b23f08dbe81a01324d26ba28149377224c61e9
ecturgut <cturgut98@gmail.com>
2021-02-11 00:18:00 +0300

    Updates for busines layer

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -13,7 +13,7 @@ namespace Business.Abstract
         IDataResult<List<Product>> GetAllByCategoryId(int id);
         IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
         IDataResult<List<ProductDetailDto>> GetProductDetails();
-        IDataResult<Product> GetById(int productId);
+        IDataResult<List<Product>> GetById(int productId);
         IResult Add(Product product);
     }
 }
