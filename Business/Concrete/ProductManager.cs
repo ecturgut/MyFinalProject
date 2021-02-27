b2cca556617a738eb338b047e086d3a883db44c1
ecturgut <cturgut98@gmail.com>
2021-02-27 21:54:18 +0300

    Added Srecured Operations Added New Tables to Database (Users,OperationClaims,UserOperationClaims)

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -28,6 +28,7 @@ namespace Business.Concrete
             _categoryService = categoryService;
         }
 
+        //[SecuredOperation("product.add")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
